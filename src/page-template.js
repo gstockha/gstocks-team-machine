553ce9e86eac3eea924d3bff850a19edffee9411
George Stockhammer <sunchipzzz@gmail.com>
2021-11-28 17:57:43 -0600

    finished html styling

--- src/page-template.js
+++ src/page-template.js
@@ -3,22 +3,49 @@ function generateMembers(memberList){
     <section id="employees">
         ${memberList.filter(({officeNumber}) => officeNumber !== undefined) //managers
             .map((obj) =>{
-                return `
-                <h3>${obj.getName()} ${obj.getRole()} ${obj.getID()} ${obj.getEmail()} ${obj.getOfficeNumber()}</h3>
+                return `<div class="card">
+                <div class="card-header">
+                <h2>${obj.getName()}</h2> <h3>${obj.getRole()}</h3></div> 
+                <div class="card-body">
+                <ul>
+                    <li>ID: ${obj.getID()}</li> 
+                    <li><a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+                    <li>Office Number: ${obj.getOfficeNumber()}</li>
+                </ul>
+                </div>
+                </div>
                 `;
             })
         .join('')}
         ${memberList.filter(({github}) => github !== undefined) //engineers
             .map((obj) =>{
-                return `
-                <h3>${obj.getName()} ${obj.getRole()} ${obj.getID()} ${obj.getEmail()} ${obj.getGitHub()}</h3>
+                return `<div class="card">
+                <div class="card-header">
+                <h2>${obj.getName()}</h2> <h3>${obj.getRole()}</h3></div>
+                <div class="card-body">
+                <ul>
+                    <li>ID: ${obj.getID()}</li> 
+                    <li><a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+                    <li>Github: <a href=${obj.getGitHub()} target="_blank">GitHub</a></li>
+                </ul>
+                </div>
+                </div>
                 `;
             })
         .join('')}
         ${memberList.filter(({school}) => school !== undefined) //interns
             .map((obj) =>{
-                return `
-                <h3>${obj.getName()} ${obj.getRole()} ${obj.getID()} ${obj.getEmail()} ${obj.getSchool()}</h3>
+                return `<div class="card">
+                <div class="card-header">
+                <h2>${obj.getName()}</h2> <h3>${obj.getRole()}</h3></div>
+                <div class="card-body">
+                <ul>
+                    <li>ID: ${obj.getID()}</li> 
+                    <li><a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+                    <li>School: ${obj.getSchool()}</li>
+                </ul>
+                </div>
+                </div>
                 `;
             })
         .join('')}
@@ -27,19 +54,67 @@ function generateMembers(memberList){
 }
 
 module.exports = memberList =>{
-    return `
-    <!DOCTYPE html>
+    return `<!DOCTYPE html>
     <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Team Profile Generator</title>
-        <link rel="stylesheet" href="../src/style.css">
-      </head>
-      <body>
-        ${generateMembers(memberList)}
-      </body>
-    </html>
-    `
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile Generator</title>
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Bungee+Shade&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: rgb(253, 253, 253);
+            font-family: Arial, Helvetica, sans-serif; 
+        }
+        .header {
+            background-color: rgb(142, 251, 233);
+            border: solid rgba(0, 0, 0, 0.267);
+            text-align: center;
+            font-size: 30px;
+        }
+        .container {
+            display: flex;
+            justify-content: space-evenly;
+        }
+        .card {
+            background-color:rgb(255, 251, 251);
+            margin-top: 4%;
+            border: solid rgba(0, 0, 0, 0.472);
+            font-size: x-large;
+            box-shadow: 10px 5px 10px rgba(0, 0, 0, 0.185);
+            border-radius: 10px;
+            min-width: 500px;
+        }
+        #employees {
+        display: flex;
+        justify-content: space-evenly;
+        flex-direction: row;
+        flex: wrap;
+        }
+        .card-header{
+            margin: 10%;
+            text-align: center;
+        }
+        .card-body{
+            margin: 10%;
+        }
+        .card ul{
+            list-style: none;
+            font-size: 24px;
+        }
+        a{
+            color: black;
+        }
+    </style>
+    </head>
+    <body>
+    <div class=header>
+        <h1>My Team</h1>
+    </div>
+    <div class="container">
+    ${generateMembers(memberList)}
+    </div>      
+    </body>
+    </html>`
 }
